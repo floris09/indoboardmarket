e795eddcd8bcc3fa4b6e9ea6719f408430b8cc22
floris09 <florismeininger@gmail.com>
2017-10-29 12:40:00 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Category.destroy_all
+Location.destroy_all
+Product.destroy_all
+
+User.create!([
+  {email:'floris@email.com',password:'123456'},
+  {email:'aad@email.com',password:'123456'},
+  {email:'jan@email.com',password:'123456'},
+  {email:'henk@email.com',password:'123456'}
+  ])
+
+surfboards = Category.create!(name:'Surfboards')
+skateboards = Category.create!(name:'Skateboards')
+bodyboards = Category.create!(name:'Bodyboards')
+swimfins = Category.create!(name:'Swimfins')
+fins = Category.create!(name:'Fins')
+leashes = Category.create!(name:'Leashes')
+tailpads = Category.create!(name:'Tailpads')
+boardbags = Category.create!(name:'Boardbags')
+clothing = Category.create!(name:'Clothing')
+wetsuits = Category.create!(name:'Wetsuits')
+cameras = Category.create!(name:'Cameras')
+others = Category.create!(name:'Others')
+
+bali = Location.create!(island:'Bali')
+sumatra = Location.create!(island:'Sumatra')
+lombok = Location.create!(island:'Lombok')
+
+Product.create! ([
+  {name:'Lost surfboard',price:3000000,category:surfboards,location:bali},
+  {name:'Fins',price:500000,category:fins,location:bali},
+  {name:'Leash',price:200000,category:leashes,location:sumatra},
+  {name:'Gopro',price:2000000,category:cameras,location:sumatra},
+  {name:'Wetsuit',price:1500000,category:wetsuits,location:lombok},
+  {name:'Sunglasses',price:1000000,category:clothing,location:lombok}
+  ])
